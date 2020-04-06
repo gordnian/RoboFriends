5dfef6d11ce69d28a76ba84b2f3c17e400c28d8a
gordnian <gordnian@gmail.com>
2020-04-05 20:12:44 -0700

    redux added to project

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,17 +1,8 @@
 import React from "react";
+import "./Scroll.css";
 
 const Scroll = (props) => {
-  return (
-    <div
-      style={{
-        overflowY: "scroll",
-        border: "5px solid black",
-        height: "800px",
-      }}
-    >
-      {props.children}
-    </div>
-  );
+  return <div className="container">{props.children}</div>;
 };
 
 export default Scroll;
